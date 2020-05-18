bc0cae821f4a5b79c199002a62dcc140fa773787
Matheus Costa <matheus.mfcosta@ibm.com>
2020-05-18 09:20:03 -0300

    first commit

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+
+import * as express from 'express'
+import * as path from 'path'
+import * as morgan from 'morgan'
+import * as http from 'http'
+
+const app = express()
+app.set('port', 8000)
+
+app.use(express.static(path.join(__dirname, '..', 'build', 'static')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+app.all('/*', (req, res, next) => {
+    console.log("!!!!!     ------  ")
+    console.log('Reading the main route through http request, sending index.html');
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+})
+
+const server = http.createServer(app)
+server.listen(app.get('port'), () => {
+    console.log("------")
+    console.log('express listening on port ' + app.get('port'))
+}) 
