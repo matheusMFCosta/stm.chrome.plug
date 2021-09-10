2803427c3a7ebf75d62eec7f1390298eda01cc18
matheusMFCosta <mf.costa@live.com>
2021-09-09 21:51:30 -0300

    multi paginas

--- bookmark/popup.js
+++ bookmark/popup.js
@@ -30,18 +30,20 @@ const getBookMarketFolders = (a) =>
         getBookMarketFolders(el.children)
     })
 
-chrome.bookmarks.getTree((a) => {
-    bookMarks = getBookMarketKeys(a).flat(Infinity)
-    getBookMarketFolders(a)
+const updateBookMarks = () => {
+    chrome.bookmarks.getTree((a) => {
+        bookMarks = getBookMarketKeys(a).flat(Infinity)
+        getBookMarketFolders(a)
 
-    const favoriteFoldersSelect = document.getElementById('favoriteFolders')
-    bookMarksFolders.forEach((el) => {
-        var option = document.createElement('option')
-        option.value = el.id
-        option.text = el.title
-        favoriteFoldersSelect.add(option)
+        const favoriteFoldersSelect = document.getElementById('favoriteFolders')
+        bookMarksFolders.forEach((el) => {
+            var option = document.createElement('option')
+            option.value = el.id
+            option.text = el.title
+            favoriteFoldersSelect.add(option)
+        })
     })
-})
+}
 
 const functionToApplyOnPage = () => {
     const elements = document.getElementsByClassName('market_listing_row_link')
@@ -60,19 +62,33 @@ const chromeExecuteScript = (script) => {
     chrome.tabs.executeScript({code: script}, () => {})
 }
 
+const addClickEventListener = (element, EventFunction) => {
+    element.addEventListener('click', EventFunction, false)
+}
+
 const loadApp = setTimeout(() => {
     var saveValueButton = document.getElementById('saveValue')
+    var refreshFavorites = document.getElementById('refreshFavorites')
     var favoriteFolders = document.getElementById('favoriteFolders')
     const defaultValue = localStorage.getItem('bm-defaultValue') || favoriteFolders.childNodes[1].value
     favoriteFolders.value = defaultValue
 
-    saveValueButton.addEventListener(
-        'click',
-        () => {
-            localStorage.setItem('bm-defaultValue', favoriteFolders.value)
-        },
-        false
-    )
+    addClickEventListener(saveValueButton, () => {
+        localStorage.setItem('bm-defaultValue', favoriteFolders.value)
+    })
+
+    addClickEventListener(refreshFavorites, () => {
+        updateBookMarks()
+
+        setTimeout(() => {
+            chromeExecuteScript(`var bookMarks = ['${bookMarks.map((el) => el.url).join("','")}'];
+        (${functionToApplyOnPage.toString()})();`)
+
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method: 'starWatch'}, function (response) {})
+            })
+        }, 100)
+    })
 
     chromeExecuteScript(`var bookMarks = ['${bookMarks.map((el) => el.url).join("','")}'];
                     (${functionToApplyOnPage.toString()})();`)
@@ -92,6 +108,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             console.log('added folder: ' + newFolder.title)
         })
     }
-
-    alert('adicionado')
 })
+
+updateBookMarks()
